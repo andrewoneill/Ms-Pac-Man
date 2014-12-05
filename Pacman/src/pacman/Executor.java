ca40fffe5a25e5ebcbed45f2ae9406d406eb082f
andrewoneill <andrew.oneill@outlook.com>
2014-12-05 03:48:10 +0000

    xml reading in correctly

--- Pacman/src/pacman/Executor.java
+++ Pacman/src/pacman/Executor.java
@@ -9,6 +9,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.Random;
+
 import pacman.controllers.Controller;
 import pacman.controllers.HumanController;
 import pacman.controllers.KeyBoardInput;
@@ -22,9 +23,9 @@ import pacman.controllers.examples.RandomNonRevPacMan;
 import pacman.controllers.examples.RandomPacMan;
 import pacman.controllers.examples.StarterGhosts;
 import pacman.controllers.examples.StarterPacMan;
+import pacman.entries.ghosts.MyGhosts;
 import pacman.game.Game;
 import pacman.game.GameView;
-
 import static pacman.game.Constants.*;
 
 /**
@@ -62,8 +63,8 @@ public class Executor
 		//run the game in asynchronous mode.
 		boolean visual=true;
 //		exec.runGameTimed(new NearestPillPacMan(),new AggressiveGhosts(),visual);
-		//exec.runGameTimed(new StarterPacMan(),new StarterGhosts(),visual);
-		exec.runGameTimed(new HumanController(new KeyBoardInput()),new AggressiveGhosts(),visual);	
+		exec.runGameTimed(new StarterPacMan(),new MyGhosts(),visual);
+		//exec.runGameTimed(new HumanController(new KeyBoardInput()),new AggressiveGhosts(),visual);	
 		//*/
 		
 		/*
--- Pacman/src/pacman/entries/ghosts/MyGhosts.java
+++ Pacman/src/pacman/entries/ghosts/MyGhosts.java
@@ -1,7 +1,18 @@
 package pacman.entries.ghosts;
 
+import java.io.File;
 import java.util.EnumMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import pacman.controllers.Controller;
+import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
@@ -13,11 +24,52 @@ import pacman.game.Game;
  */
 public class MyGhosts extends Controller<EnumMap<GHOST,MOVE>>
 {
+	public static final int CROWDED_DISTANCE = 40;
+	public static final int PACMAN_DISTANCE = 20;
+	public static final int PILL_PROXIMITY = 20;
+	
 	private EnumMap<GHOST, MOVE> myMoves=new EnumMap<GHOST, MOVE>(GHOST.class);
+	
+	private MOVE[] moves = MOVE.values();
+	private String myState = "chase";
+	private String myEvent= "edible";
+	private String myAction = "chase";
+
+	public void FSM() {
+		try {
+			File fXmlFile = new File(
+					"XML's/fsm.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+
+			(doc).getDocumentElement().normalize();
+
+			NodeList nList = doc.getElementsByTagName("state");
+
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);
+				Element eElement = (Element) nNode;
+				System.out.println();
+				if (eElement.getElementsByTagName("currentstate").item(0).getTextContent().equals(myState)
+						&& eElement.getElementsByTagName("event").item(0).getTextContent().equals(myEvent)) {
+					myAction = eElement.getElementsByTagName("action").item(0).getTextContent();
+					myState = eElement.getElementsByTagName("newstate").item(0).getTextContent();
+					System.out.println(myState);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 	
 	public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue)
 	{
 		myMoves.clear();
+		
+		FSM();
 		
 		//Place your game logic here to play the game as the ghosts
 		
